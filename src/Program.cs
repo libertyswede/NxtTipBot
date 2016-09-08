0f5f2920d66d1e74375d3bf37b4c6994dcf3e7f2
Liberty Swede <libertyswede@outlook.com>
2016-09-09 00:06:38 +0200

    added dev config file

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.PlatformAbstractions;
 using Newtonsoft.Json.Linq;
 
 namespace NxtTipBot
@@ -12,20 +16,33 @@ namespace NxtTipBot
     {
         public static void Main(string[] args)
         {
+            var configSettings = ReadConfig();
+            var apiToken = configSettings.Single(c => c.Key == "apitoken").Value;
             var program = new Program();
-            Task.Run(() => program.Run()).Wait();
+            Task.Run(() => program.Run(apiToken)).Wait();
             Console.WriteLine("Done!");
         }
 
-        private async Task Run()
+        private static IEnumerable<IConfigurationSection> ReadConfig()
+        {
+            var configBuilder = new ConfigurationBuilder();
+            configBuilder.SetBasePath(PlatformServices.Default.Application.ApplicationBasePath);
+            configBuilder.AddJsonFile("config.json");
+            configBuilder.AddJsonFile("config-Development.json", true);
+            var configRoot = configBuilder.Build();
+            var configSettings = configRoot.GetChildren();
+            return configSettings;
+        }
+
+        private async Task Run(string apiToken)
         {
             string websocketUri = string.Empty;
             using (var httpClient = new HttpClient())
-            using (var response = await httpClient.GetAsync("https://slack.com/api/rtm.start?token=xxxxxxx"))
+            using (var response = await httpClient.GetAsync($"https://slack.com/api/rtm.start?token={apiToken}"))
             using (var content = response.Content)
             {
                 var json = await content.ReadAsStringAsync();
-                Console.WriteLine(json);
+                // Console.WriteLine(json);
                 var jObject = JObject.Parse(json);
                 websocketUri = (string)jObject["url"];
             }
