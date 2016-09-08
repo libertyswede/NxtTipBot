efb2d420f266d898520d5737512db1c8ec40cc42
Liberty Swede <libertyswede@outlook.com>
2016-09-09 00:20:55 +0200

    refactor

--- src/Program.cs
+++ src/Program.cs
@@ -1,14 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Net.WebSockets;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.PlatformAbstractions;
-using Newtonsoft.Json.Linq;
 
 namespace NxtTipBot
 {
@@ -18,9 +13,8 @@ namespace NxtTipBot
         {
             var configSettings = ReadConfig();
             var apiToken = configSettings.Single(c => c.Key == "apitoken").Value;
-            var program = new Program();
-            Task.Run(() => program.Run(apiToken)).Wait();
-            Console.WriteLine("Done!");
+            var connector = new SlackConnector(apiToken);
+            Task.Run(() => connector.Run()).Wait();
         }
 
         private static IEnumerable<IConfigurationSection> ReadConfig()
@@ -32,54 +26,6 @@ namespace NxtTipBot
             var configRoot = configBuilder.Build();
             var configSettings = configRoot.GetChildren();
             return configSettings;
-        }
-
-        private async Task Run(string apiToken)
-        {
-            string websocketUri = string.Empty;
-            using (var httpClient = new HttpClient())
-            using (var response = await httpClient.GetAsync($"https://slack.com/api/rtm.start?token={apiToken}"))
-            using (var content = response.Content)
-            {
-                var json = await content.ReadAsStringAsync();
-                // Console.WriteLine(json);
-                var jObject = JObject.Parse(json);
-                websocketUri = (string)jObject["url"];
-            }
-
-            var webSocket = new ClientWebSocket();            
-            await webSocket.ConnectAsync(new System.Uri(websocketUri), CancellationToken.None);
-            await Recieve(webSocket);
-        }
-
-        private async Task Recieve(ClientWebSocket webSocket)
-        {
-            var buffer = new byte[1024];
-            while (webSocket.State == WebSocketState.Open)
-            {
-                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                if (result.MessageType == WebSocketMessageType.Close)
-                {
-                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
-                }
-                else
-                {
-                    var encoder = new UTF8Encoding();
-                    var json = encoder.GetString(buffer, 0, result.Count);
-                    var jObject = JObject.Parse(json);
-                    var type = (string)jObject["type"];
-                    switch (type)
-                    {
-                        case "hello": // ignore
-                            break;
-                        case "reconnect_url": // ignore
-                            break;
-                        default: Console.WriteLine(json);
-                            break; 
-                    }
-                    
-                }
-            }
         }
     }
 }
---
+++ src/SlackConnector.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace NxtTipBot
+{
+    public class SlackConnector
+    {
+        private readonly string apiToken;
+
+        public SlackConnector(string apiToken)
+        {
+            this.apiToken = apiToken;
+        }
+
+        public async Task Run()
+        {
+            string websocketUri = string.Empty;
+            using (var httpClient = new HttpClient())
+            using (var response = await httpClient.GetAsync($"https://slack.com/api/rtm.start?token={apiToken}"))
+            using (var content = response.Content)
+            {
+                var json = await content.ReadAsStringAsync();
+                // Console.WriteLine(json);
+                var jObject = JObject.Parse(json);
+                websocketUri = (string)jObject["url"];
+            }
+
+            var webSocket = new ClientWebSocket();            
+            await webSocket.ConnectAsync(new System.Uri(websocketUri), CancellationToken.None);
+            await Recieve(webSocket);
+        }
+
+        private async Task Recieve(ClientWebSocket webSocket)
+        {
+            var buffer = new byte[1024];
+            while (webSocket.State == WebSocketState.Open)
+            {
+                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                }
+                else
+                {
+                    var encoder = new UTF8Encoding();
+                    var json = encoder.GetString(buffer, 0, result.Count);
+                    var jObject = JObject.Parse(json);
+                    var type = (string)jObject["type"];
+                    switch (type)
+                    {
+                        case "hello": Console.WriteLine("Hello recieved.");
+                            break;
+                        case "presence_change": // ignore
+                        case "reconnect_url":
+                            break;
+                        default: Console.WriteLine(json);
+                            break; 
+                    }
+                }
+            }
+        }
+    }
+}
