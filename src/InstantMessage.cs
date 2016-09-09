191d43e4f63cafdf1a2d8e7d0b6881b669e62047
Liberty Swede <libertyswede@outlook.com>
2016-09-09 23:18:33 +0200

    cleanup of unused properties

--- src/InstantMessage.cs
+++ src/InstantMessage.cs
@@ -1,13 +1,8 @@
-using Newtonsoft.Json;
-
 namespace NxtTipBot
 {
     public class InstantMessage
     {
         public string Id { get; set; }
         public string User { get; set; }
-
-        [JsonProperty(PropertyName = "is_open")]
-        public bool IsOpen { get; set; }
     }
 }
