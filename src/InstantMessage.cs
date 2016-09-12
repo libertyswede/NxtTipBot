89c2e9dca7adb5c13cbdc0b75f45d87ae4da71b0
Liberty Swede <libertyswede@outlook.com>
2016-09-12 22:29:01 +0200

    bugfix

--- src/InstantMessage.cs
+++ src/InstantMessage.cs
@@ -1,8 +1,12 @@
+using Newtonsoft.Json;
+
 namespace NxtTipbot
 {
     public class InstantMessage
     {
         public string Id { get; set; }
-        public string User { get; set; }
+
+        [JsonProperty(PropertyName = "user")]
+        public string UserId { get; set; }
     }
 }
