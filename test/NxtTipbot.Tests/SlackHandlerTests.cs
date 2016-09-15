b4e3e6fffffbb9fa32b26f1151fef5185d792872
Liberty Swede <libertyswede@outlook.com>
2016-09-15 22:53:17 +0200

    test for balance command

--- test/NxtTipbot.Tests/SlackHandlerTests.cs
+++ test/NxtTipbot.Tests/SlackHandlerTests.cs
@@ -6,24 +6,44 @@ namespace NxtTipbot.Tests
 {
     public class SlackHandlerTests
     {
+        private readonly Mock<INxtConnector> nxtConnectorMock = new Mock<INxtConnector>(); 
+        private readonly Mock<IWalletRepository> walletRepositoryMock = new Mock<IWalletRepository>();
+        private readonly Mock<ILogger> loggerMock = new Mock<ILogger>();
+        private readonly Mock<ISlackConnector> slackConnectorMock = new Mock<ISlackConnector>();
+        private readonly SlackHandler slackHandler;
+        private readonly InstantMessage instantMessage = new InstantMessage { Id = "Id", UserId = "UserId" };
+        private readonly User user = new User { Id = "UserId", Name = "XunitBot" };
+
+        public SlackHandlerTests()
+        {
+            slackHandler = new SlackHandler(nxtConnectorMock.Object, walletRepositoryMock.Object, loggerMock.Object);
+            slackHandler.SlackConnector = slackConnectorMock.Object;
+        }
+
         [Theory]
         [InlineData("help")]
         [InlineData(" HELP ")]
         [InlineData("hElP ")]
-        public async void SendHelpText(string command)
+        public async void Help(string command)
         {
-            var nxtConnectorMock = new Mock<INxtConnector>();
-            var walletRepositoryMock = new Mock<IWalletRepository>();
-            var loggerMock = new Mock<ILogger>();
-            var slackConnectorMock = new Mock<ISlackConnector>();
-            var slackHandler = new SlackHandler(nxtConnectorMock.Object, walletRepositoryMock.Object, loggerMock.Object);
-            slackHandler.SlackConnector = slackConnectorMock.Object;
-            var instantMessage = new InstantMessage { Id = "Id", UserId = "UserId" };
-
-            await slackHandler.InstantMessageRecieved(command, null, instantMessage);
+            await slackHandler.InstantMessageRecieved(command, user, instantMessage);
 
             slackConnectorMock.Verify(c => c.SendMessage(instantMessage.Id, 
                 It.Is<string>(input => input.StartsWith("*Direct Message Commands*")), true));
+        }
+        
+        [Theory]
+        [InlineData("balance")]
+        [InlineData(" BALANCE ")]
+        [InlineData("bAlAnCe ")]
+        public async void BalanceShouldReturnNoAccount(string command)
+        {
+            walletRepositoryMock.Setup(r => r.GetAccount(It.IsAny<string>())).ReturnsAsync(null);
+
+            await slackHandler.InstantMessageRecieved(command, user, instantMessage);
+
+            slackConnectorMock.Verify(c => c.SendMessage(instantMessage.Id, 
+                It.Is<string>(input => input.StartsWith("You do currently not have an account")), true));
         }
     }
 }
