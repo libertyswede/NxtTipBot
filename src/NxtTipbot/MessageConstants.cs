b4cfa7c02247c5b7e6a64954f61ec8cc333e8b1f
Liberty Swede <libertyswede@outlook.com>
2016-09-25 23:59:51 +0200

    updated deposit message

--- src/NxtTipbot/MessageConstants.cs
+++ src/NxtTipbot/MessageConstants.cs
@@ -37,7 +37,7 @@ namespace NxtTipbot
 
         public static string DepositAddress(string accountRs)
         {
-            return $"You can deposit NXT here: {accountRs}";
+            return $"You may deposit Nxt or accepted currencies/assets here: {accountRs}";
         }
 
         public static string NotEnoughFunds(decimal balance, string unit)
